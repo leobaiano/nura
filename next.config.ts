7abb5ef5ab54ee63ed70019d511e61a3950c4605
Leo Baiano <ljunior2005@gmail.com>
2026-09-18 18:10:35 -0300

    chore: configura github actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/nura", 
+  assetPrefix: "/nura",
   images: {
     unoptimized: true,
   },
